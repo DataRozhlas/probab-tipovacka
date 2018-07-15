e442f0e264f83ef09a4cd0e4d3228fd42f2366c1
timichal <michal@zlatkovsky.cz>
2018-07-15 15:17:31 +0200

    backend

--- article.md
+++ article.md
@@ -1,39 +1,19 @@
-title: "Data z mobilů:<br>Praha se denně nafoukne o polovinu, v centru jsou návštěvníci v převaze"
-perex: "Skoro 140 tisíc lidí přijede do Prahy každý den za prací. Míří hlavně do kancelářských budov a do centra města. S nimi pak dorazí i přes půl milionu návštěvníků. Ukázala to analýza dat o pohybu mobilních telefonů, kterou si nyní město nechává zpracovat."
-published: "21. dubna 2018"
+title: "Ço znamená 'možná' nebo 'pravděpodobně'? Řekněte nám to"
+perex: "Inspirujeme se americým průzkumem, který zjišťoval, jak pravděpodobná jsou slova. Zkuste si to s námi!"
+published: "17. července 2018"
 coverimg: https://interaktivni.rozhlas.cz/brexit/media/cover.jpg
 coverimg_note: "Foto <a href='#'>ČTK</a>"
-styles: []
-# snadné načítání csv: d3csv v libraries, d3.csv("soubor.csv").then(function(data){} ) v kódu
-libraries: [] #jquery, d3, d3v5, d3csv, highcharts, datatables
+styles: ["https://interaktivni.rozhlas.cz/data/dhondtator/jquery-ui.min.css"]
+libraries: [jquery, "https://interaktivni.rozhlas.cz/data/dhondtator/jquery-ui.min.js", "https://code.highcharts.com/highcharts.js"] #jquery, d3, d3v5, d3csv, highcharts, datatables
 options: [noheader] #wide, noheader (, nopic)
 ---
-<left>
-	<p>
-	<b>KAREL HYNEK MÁCHA</b>
-	</p><p>
-	Karel Hynek Mácha (16. listopadu 1810 Praha-Malá Strana[1] – 6. listopadu 1836 Litoměřice[2]) byl český básník a prozaik, představitel českého romantismu a zakladatel moderní české poezie. Proslavil se jak svým životem, tak dílem, jemuž dominuje Máj (1836).
-	</p>
-</left>
 
-Během dne se lidé v Praze soustřeďují v okolí administrativních center a obchodů, po poledni se tak nejvíc zahustí okolí Andělu, centrum Prahy (Vodičkova ulice a Petrské náměstí) a Brumlovka nedaleko Budějovické. Na poslední jmenované adrese sídlí řada velkých korporací, mezi nimi i Microsoft nebo ČEZ. Pro srovnání, na zmíněných místech je ve „špičce“ okolo šesti stovek osob na jeden hektar, průměr Prahy je 25 obyvatel na [hektar](https://cs.wikipedia.org/wiki/Hektar) (Václavské náměstí má rozlohu asi 4 hektary).
+asi, snad, nejspíš, jistě, určitě, možná, pravděpodobně, každopádně, rozhodně, spíše
 
-Naopak v noci se nejvíc lidí se „namačká“ ve Vršovicích, jde o 430 osob na hektar.
-
-Detaily si můžete prohlédnout v následující mapě, kterou z dat mobilních operátorů zpracoval pražský [Institut plánování a rozvoje](http://www.iprpraha.cz/).
-
-<wide>_Pokud vás zajímá, kolik lidí bývá ve dne a v noci ve vaší čtvrti, najděte si ji na mapě, přepněte se na časové řezy, vyberte přepínač hustota osob a potom pohybujte posuvníkem nahoře._</wide>
-
+<div id="quiz">
+</div>
 
 ## Data na prodej
-<right>
-	<p>
-	<b>KAREL HYNEK MÁCHA</b>
-	</p><p>
-	Karel Hynek Mácha (16. listopadu 1810 Praha-Malá Strana[1] – 6. listopadu 1836 Litoměřice[2]) byl český básník a prozaik, představitel českého romantismu a zakladatel moderní české poezie. Proslavil se jak svým životem, tak dílem, jemuž dominuje Máj (1836).
-	</p>
-</right>
-
 I pokud netelefonujeme nebo neposíláme SMS, telefon pravidelně komunikuje s vysílači v síti mobilního operátora. Z dat o síle signálu a použité anténě je možné odvodit, kde se přibližně telefon nachází a jak dlouhou dobu na místě strávil.
 
 Mobilní operátor má [zákonnou povinnost](https://www.zakonyprolidi.cz/cs/2005-127/zneni-20160919#p97-3) takové informace uchovávat, a to půl roku do minulosti, přistupovat k nim ale mohou jen bezpečnostní složky, typicky policie nebo rozvědka.
--- js/script.js
+++ js/script.js
@@ -1,1 +1,166 @@
-// JS soubory ve složce /styles/ se do výsledného článku zakompilují automaticky
+"use strict";
+
+// responsive slider bazmek
+
+!function (a) {
+    function f(a, b) {
+        if (!(a.originalEvent.touches.length > 1)) {
+            a.preventDefault();var c = a.originalEvent.changedTouches[0],
+                d = document.createEvent("MouseEvents");d.initMouseEvent(b, !0, !0, window, 1, c.screenX, c.screenY, c.clientX, c.clientY, !1, !1, !1, !1, 0, null), a.target.dispatchEvent(d);
+        }
+    }if (a.support.touch = "ontouchend" in document, a.support.touch) {
+        var e,
+            b = a.ui.mouse.prototype,
+            c = b._mouseInit,
+            d = b._mouseDestroy;b._touchStart = function (a) {
+            var b = this;!e && b._mouseCapture(a.originalEvent.changedTouches[0]) && (e = !0, b._touchMoved = !1, f(a, "mouseover"), f(a, "mousemove"), f(a, "mousedown"));
+        }, b._touchMove = function (a) {
+            e && (this._touchMoved = !0, f(a, "mousemove"));
+        }, b._touchEnd = function (a) {
+            e && (f(a, "mouseup"), f(a, "mouseout"), this._touchMoved || f(a, "click"), e = !1);
+        }, b._mouseInit = function () {
+            var b = this;b.element.bind({ touchstart: a.proxy(b, "_touchStart"), touchmove: a.proxy(b, "_touchMove"), touchend: a.proxy(b, "_touchEnd") }), c.call(b);
+        }, b._mouseDestroy = function () {
+            var b = this;b.element.unbind({ touchstart: a.proxy(b, "_touchStart"), touchmove: a.proxy(b, "_touchMove"), touchend: a.proxy(b, "_touchEnd") }), d.call(b);
+        };
+    }
+}(jQuery);
+
+$(function () {
+    var stamp = Date.now() + Math.floor(Math.random() * 10000);
+
+    // otazky
+    var headlines = { "q1": "Cizinci mezi vězni", "q2": "Potraty", "q3": "Chytré telefony", "q4": "Adopce homosexuálními páry", "q5": "Sociální dávky", "q6": "Alkohol", "q7": "Romové", "q8": "Životní spokojenost" };
+    var questions = { "q1": "Kolik procent odsouzených v českých věznicích jsou cizinci?",
+        "q2": "Kolik umělých přerušení těhotenství v Česku připadá na 100 živě narozených dětí?",
+        "q3": "Kolik procent Čechů starších 16 let používá internet v mobilu?",
+        "q4": "Kolik Čechů ze sta si myslí, že homosexuální páry by měly mít právo adoptovat děti z dětských domovů?",
+        "q5": "Jaký podíl státního rozpočtu se loni vydal na sociální dávky - podporu v nezaměstnanosti a pomoc v hmotné nouzi?",
+        "q6": "Kolik procent Čechů ve věku 15-64 let pije alkohol tolik, že se nachází v kategorii rizikové konzumace?",
+        "q7": "Jak velký podíl Čechů hodnotí negativně soužití s Romy?",
+        "q8": "Kolik procent Čechů je spokojených se svým životem?"
+    };
+
+    for (var i = 1; i <= 8; i++) {
+        $("#quiz").append('<h3>' + i + '. ' + headlines["q" + i] + '</h3>');
+        $("#quiz").append('<div>' + questions["q" + i] + '</b>');
+        $("#quiz").append('<div id="q' + i + '" class="slider"><div id="q' + i + 'handle" class="ui-slider-handle handle"></div></div>');
+        $("#quiz").append('<div class="button disabled" id="buttonq' + i + '">Potvrdit</div>');
+        $("#quiz").append('<div id="chartq' + i + '" style="width: 100%; margin: 0 auto"></div>');
+        $("#quiz").append('<div class="result" id="resq' + i + '"></div>');
+
+        $("#q" + i).slider({
+            min: 0,
+            max: 100,
+            step: 1,
+            value: 50,
+            create: function create() {
+                $(this).children().text($(this).slider("value"));
+            },
+            slide: function slide(event, ui) {
+                $(this).children().text(ui.value);
+                $(this).next().removeClass("disabled");
+            }
+        });
+    };
+
+    $(".button").click(function () {
+        if (!$(this).hasClass("disabled")) {
+
+            var qid = $(this).prev().attr('id');
+            var val = $("#" + qid + "handle").text();
+            $(this).text("Čekejte...");
+            $.ajax({
+                url: "https://lvehmkssr1.execute-api.eu-central-1.amazonaws.com/prod",
+                type: "POST",
+                crossDomain: !0,
+                contentType: "application/json",
+                data: JSON.stringify({ "qid": qid, "val": val }),
+                dataType: "json",
+                success: function success(response) {
+                    delete response.qid;
+                    console.log(response);
+                    $("#" + qid).css("display", "none");
+                    $("#button" + qid).css("display", "none");
+                    
+                    var total = 0;
+                    var sum = 0;
+                    for (i in response) {
+                        total += response[i];
+                        sum += i * response[i]
+                    }
+                    var avg = sum/total;
+                    for (i in response) {
+                        response[i] = response[i] / total * 100;
+                    }
+
+                    for (i = 1; i <= 100; i++) {
+                        if (!(i in response)) {
+                            response[i] = 0;
+                        };
+                    };
+
+                    var data = Object.keys(response).map(function(e) {
+                      return response[e]
+                    })
+                    $("#chart" + qid).animate({ height: "200px" });
+                    var chart = Highcharts.chart('chart' + qid, {
+                        chart: {
+                            type: 'column',
+                            height: 200
+                        },
+                        title: {
+                            text: null
+                        },
+                        xAxis: {
+                            crosshair: true,
+                            plotLines: [{
+                                color: "darkblue",
+                                dashStyle: "dash",
+                                width: 2,
+                                value: val
+                            }]
+                        },
+                        legend: {
+                            labelFormat: "<span id='darkbluedot'></span>Váš tip",
+                            useHTML: true
+                        },
+                        credits: {
+                            enabled: false
+                        },
+                        yAxis: {
+                            min: 0,
+                            title: {
+                                text: 'Podíl hlasů'
+                            },
+                            labels: {
+                                format: "{value} %"
+                            }
+                        },
+                        plotOptions: {
+
+                            column: {
+                                pointPadding: 0.1,
+                                borderWidth: 0,
+                                events: {
+                                    legendItemClick: function () {
+                                        return false; 
+                                    }
+                                }
+                            }
+                        },
+                        tooltip: {
+                            enabled: false
+                        },
+                        series: [{
+                            name: 'otázka xy',
+                            data: data
+                        }]
+                    });
+                    var pct = " %";
+                    $("#res" + qid).html("Váš tip byl <b>" + val + pct + "</b>. Čtenáři v průměru tipují <b>" + Math.round(avg) + pct + "</b>.");
+                }
+            });
+        }
+    });
+});
